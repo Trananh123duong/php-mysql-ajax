9f4ce4dbacb22977a59badbf186c46b5d7d4b12a
dev05 <dev05@dev.co-mit.com>
2024-12-16 14:34:05 +0700

    list record table tbl_khachhang

--- src/ajax_action.php
+++ src/ajax_action.php
@@ -12,4 +12,42 @@ if (isset($_POST['hovaten'])) {
     $result = mysqli_query($con, "insert into tbl_khachhang (hovaten,sophone,diachi,email,ghichu) values ('$hovaten','$sophone','$diachi','$email','$ghichu')");
 }
 
+$output = '';
+$sql_select = mysqli_query($con, 'select * from tbl_khachhang order by khachhang_id desc');
+$output .= '
+    <div class="table table-responsive">
+        <table class="table table-bordered">
+            <tr>
+                <td>Họ và tên</td>
+                <td>Phone</td>
+                <td>Email</td>
+                <td>Địa chỉ</td>
+                <td>Ghi chú</td>
+            </tr>
+';
+if (mysqli_num_rows($sql_select) > 0) {
+    while ($rows = mysqli_fetch_array($sql_select)) {
+        $output .= '
+            <tr>
+                <td> ' . $rows['hovaten'] . '</td>
+                <td> ' . $rows['sophone'] . '</td>
+                <td> ' . $rows['email'] . '</td>
+                <td> ' . $rows['diachi'] . '</td>
+                <td> ' . $rows['ghichu'] . '</td>
+            </tr>
+        ';
+    }
+} else {
+    $output .= '
+        <tr>
+            <td colspan="5">Dữ liệu chưa có</td>
+        </tr>
+    ';
+}
+$output .= '
+        </table>
+    </div>
+';
+
+echo $output;
 ?>
--- src/index.php
+++ src/index.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
+
 <body>
     <div class="container">
         <div class="col-md-12">
@@ -25,28 +28,49 @@
                 <br>
                 <input type="button" name="insert_data" id="button_insert" value="Insert" class="btn btn-success">
             </form>
-        </div> 
+
+            <h3>Load dữ liệu bảng bằng ajax</h3>
+            <div id="load_data">
+
+            </div>
+        </div>
     </div>
     <script type="text/javascript">
-        $('#button_insert').on('click', function() {
-            var hovaten = $('#hovaten').val();
-            var sophone = $('#sophone').val();
-            var diachi = $('#diachi').val();
-            var email = $('#email').val();
-            var ghichu = $('#ghichu').val();
-            if (hovaten == '' || sophone == '' || diachi == '' || email == '' || ghichu == '') {
-                alert('rong')
-            } else {
+        $(document).ready(function () {
+            function fetch_data() {
                 $.ajax({
                     url: "ajax_action.php",
                     method: "POST",
-                    data: {hovaten: hovaten, sophone: sophone, diachi: diachi, email: email, ghichu: ghichu},
-                    success:function(data) {
-                        alert('Insert success');
-                        $('#insert_data_hoten')[0].reset();
+                    success: function (data) {
+                        $('#load_data').html(data);
+                        fetch_data();
                     }
                 });
             }
+
+            fetch_data();
+
+            $('#button_insert').on('click', function () {
+                var hovaten = $('#hovaten').val();
+                var sophone = $('#sophone').val();
+                var diachi = $('#diachi').val();
+                var email = $('#email').val();
+                var ghichu = $('#ghichu').val();
+                if (hovaten == '' || sophone == '' || diachi == '' || email == '' || ghichu == '') {
+                    alert('rong')
+                } else {
+                    $.ajax({
+                        url: "ajax_action.php",
+                        method: "POST",
+                        data: { hovaten: hovaten, sophone: sophone, diachi: diachi, email: email, ghichu: ghichu },
+                        success: function (data) {
+                            alert('Insert success');
+                            $('#insert_data_hoten')[0].reset();
+                            fetch_data();
+                        }
+                    });
+                }
+            });
         });
     </script>
 
@@ -55,4 +79,5 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"></script>
 
 </body>
+
 </html>
